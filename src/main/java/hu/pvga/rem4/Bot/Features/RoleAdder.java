79d26b1c49637abedd9079dbfae5b83ace3ec9cb
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2022-05-01 20:12:31 +0200

    RoleAdder feature

---
+++ src/main/java/hu/pvga/rem4/Bot/Features/RoleAdder.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022.
+ * Author: aryxs3m
+ * All rights reserved.
+ */
+
+package hu.pvga.rem4.Bot.Features;
+
+import hu.pvga.rem4.Config.ConfigManager;
+import hu.pvga.rem4.Config.RoleAdderConfig;
+import net.dv8tion.jda.api.entities.UserSnowflake;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+/**
+ * RoleAdder
+ *
+ * This feature will add a role to new users when them joining your server.
+ */
+public class RoleAdder extends BaseFeature {
+    RoleAdderConfig roleAdderConfig;
+
+    public RoleAdder() throws IOException {
+        roleAdderConfig = ConfigManager.load(RoleAdderConfig.class);
+    }
+
+    @Override
+    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+        try {
+            event.getGuild().addRoleToMember(
+                    UserSnowflake.fromId(event.getMember().getId()),
+                    event.getGuild().getRolesByName(roleAdderConfig.getRoleName(), false).get(0)
+            ).queue();
+            System.out.println("Added " + roleAdderConfig.getRoleName() + " role to " + event.getMember().getId());
+        } catch (Exception e) {
+            logger.error("Cannot add role to new member", e);
+        }
+    }
+}
---
+++ src/main/java/hu/pvga/rem4/Config/RoleAdderConfig.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022.
+ * Author: aryxs3m
+ * All rights reserved.
+ */
+
+package hu.pvga.rem4.Config;
+
+public class RoleAdderConfig extends BaseConfig {
+    private String roleName;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
